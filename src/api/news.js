f5d5026fc903f63f4a57a92b1afefa7d05f9d7d9
Ruslan Bilyshko <ruslan.bilyshko@gmail.com>
2023-02-19 14:16:34 +0200

    Fix nock data

--- src/api/news.js
+++ src/api/news.js
@@ -1,32 +1,30 @@
-const mockData = {
-    title: 'News title',
-    preview: 'news/News-Preview.jpg',
-    summary: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-}
-
-
+import { faker } from '@faker-js/faker'
 export const mockNewsCollection = () => {
+    console.log('Make mock News collection')
+
     const collection = []
 
     for (let i = 0; i < 10; i++) {
         collection.push({
-            ...mockData,
-            id: i + 1,
-            title: `${mockData.title} id: ${i + 1}`
+            id: faker.datatype.bigInt(),
+            preview: faker.image.business(),
+            title: faker.lorem.words(10),
+            summary: faker.lorem.words(40)
         })
     }
 
     return collection
 }
 
+const mockData = mockNewsCollection()
+
 export const getNewsCollection = (params) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve({
                 success: 1,
-                data: mockNewsCollection()
+                data: mockData
             });
-        }, 2000);
+        }, 100);
     })
 }
