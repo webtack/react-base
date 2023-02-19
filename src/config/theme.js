6e0b39e4f147285d9bb1e3ad044ad91446f746fe
Ruslan Bilyshko <ruslan.bilyshko@gmail.com>
2023-02-19 13:25:11 +0200

    Fix source fonts

--- src/config/theme.js
+++ src/config/theme.js
@@ -19,7 +19,8 @@ export default {
         "fontSize": 14,
         "fontWeightLight": 300,
         "fontWeightRegular": 400,
-        "fontWeightMedium": 500
+        "fontWeightMedium": 500,
+        "fontWeightBold": 700
     },
     breakpoints: {
         values: {
